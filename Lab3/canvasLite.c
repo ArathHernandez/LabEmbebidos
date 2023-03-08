6b6c8351411052967f7841223071d6f5b12bb912
ArathHernandez <47037125+ArathHernandez@users.noreply.github.com>
2023-03-07 20:24:11 -0600

    Update canvasLite.c

--- Lab3/canvasLite.c
+++ Lab3/canvasLite.c
@@ -6,27 +6,27 @@ int main(int argc, char *argv[]){
   FILE *f = fopen("file.txt", "w");
   
   if(f == NULL){
-    fprintf(sdterr, "Error opening file!");
+    fprintf(sdterr, "Error opening file!\n");
       exit(1);
   }
   
   for(int i = 0; i <atoi(argv[2]); i++){
     char Nombre[100], ID[30];
-    printf("Enter Name:");
+    printf("Enter Name:\n");
     scanf("%s", Nombre);
-    printf("Enter ID:");
+    printf("Enter ID:\n");
     scanf("%s", ID);
-    printf("Do you wish to add more [y/n]:");
+    printf("Do you wish to add more [y/n]:\n");
     char rsp;
     scanf(" %c", &rsp);
     
-    fprintf(f, "%s, t%sn, Nombre, ID);
+    fprintf(f, "%s, \t%s\n, Nombre, ID);
     if(rsp == 'n'){
       break;
     }
    
             
-  printf("Students information stored in Datalog.txt");
+  printf("Students information stored in Datalog.txt\n");
   //const char *text = "Write this to the file";
   //fprintf(f, "Some text: %s", text);
   fclose(f);
